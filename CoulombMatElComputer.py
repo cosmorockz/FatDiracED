ecb79c0d2dafb9c46223c7a4949d536a4da8233d
Arjun Dey <arjundey123456789@gmail.com>
2023-01-17 14:43:56 +0000

    modified:   CoulombMatElComputer.py

--- CoulombMatElComputer.py
+++ CoulombMatElComputer.py
@@ -5,8 +5,50 @@ Coulomb interactions. Given any Dirac cutoff (specifies )
 
 import numpy as np
 
-cutoff = 1
-Q = 4
+cutoff = 1 # Landau level index cutoff
+Q = 4 # Dirac magnetic Monopole charge
+
+l_max = Q - 0.5 + cutoff # Maximum Landau level for the given Magnetic
+# field and "cutoff"
+
+def Bset(Q,cutoff,m):
+    # Set of quantum numbers if magentic 
+    # field and cutoff is specified for a given m
+    levels = []
+    if ((Q*2) % 2) < 1e-8:
+#         M = max(int(abs(m)+0.5),)
+        for i in range(-cutoff,cutoff+1):
+            l = Q + np.abs(i) - 1/2
+            if ((np.abs(m)*2) % 2) > 0.999:
+                if np.abs(m) <= np.abs(l):
+                    levels.append(i)
+    else:
+        for i in range(-cutoff,cutoff+1):
+            l = Q + np.abs(i) - 1/2
+            if ((np.abs(m)*2) % 2) < 1e-8:
+                if np.abs(m) <= np.abs(l):
+                    levels.append(i)
+    return levels
+
+
+MM = [-l_max + i for i in range(2*l_max + 1)] # All the allowed m's
+
+StatesAll = {} # Dictionary of all the allowed states
+
+k = 0 # Key for the dictionary
+for m in MM:
+    AllowedLevels = Bset(Q,cutoff,m)
+    for level in AllowedLevels:
+        la = np.sign(level) # Lambda
+        n = np.abs(level) # Landau level index
+        StatesAll[str(k)] = {}
+        StatesAll[str(k)]['m'] = m 
+        StatesAll[str(k)]['n'] = n 
+        StatesAll[str(k)]['lambda'] = la
+        k += 1
+
+
+
 
 
 
